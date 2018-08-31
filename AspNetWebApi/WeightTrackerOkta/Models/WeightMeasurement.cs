0dc39294b69a2715e19d83b1c0d0ed27210aa42b
Ibro <ibro@ibro.me>
2018-08-31 10:12:14 +0200

    update model

--- AspNetWebApi/WeightTrackerOkta/Models/WeightMeasurement.cs
+++ AspNetWebApi/WeightTrackerOkta/Models/WeightMeasurement.cs
@@ -1,20 +1,15 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace WeightTrackerOkta.Models
 {
     public class WeightMeasurement
     {
-        [Key]
         public int Id { get; set; }
 
-        [Required]
-        public float Value { get; set; }
+        public string Name { get; set; }
 
-        [Required]
-        public string Description { get; set; }
+        public decimal Value { get; set; }
 
-        [Required]
-        public DateTime MeasuredAt { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
